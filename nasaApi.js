5c745b139c329e89cae9a35eeff31e16aabba619
Jan Jacek <bobkiewicz.jacek@gmail.com>
2022-07-26 14:35:18 +0200

    update axios rek with date prams

--- nasaApi.js
+++ nasaApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const nasaMsg = async () => {
     const getResponse = await axios
-      .get("https://api.nasa.gov/planetary/apod?api_key=aRmGij9Qh1oahMdOAYjkeqrAGaiyVoryOztfE1ez")
+      .get("https://api.nasa.gov/planetary/apod?api_key=aRmGij9Qh1oahMdOAYjkeqrAGaiyVoryOztfE1ez",{ params:{date:"2020-05-05"}})
       .then(res => res)
       .catch(err => console.log(err));
     return getResponse;
